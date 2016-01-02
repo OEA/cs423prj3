d7df6abcfa9e2243744b1ef8464283f28e4abaf4
Ömer Aslan <omer.aslan@ozu.edu.tr>
2016-01-02 18:41:41 +0200

    changes

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,6 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/features2d.hpp>
 #include "opencv2/xfeatures2d.hpp"
 #include <vector>
 
@@ -11,6 +10,150 @@
 using namespace std;
 using namespace cv;
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+void trainBOW() {
+    
+    //To store all the descriptors that are extracted from all the images.
+    Mat featuresUnclustered;
+    for(int j=1; j<500;j++){
+        
+        Mat imgDeneme;
+        string trainPath = "/Users/omer/Downloads/project3-data/train/bedroom";
+        
+        string num = "";//itoa(j,numstr,10);
+        if(j<10){
+            stringstream sstm;
+            sstm << num << j;
+            num = "000"+sstm.str();
+        }else if (j < 100){
+            stringstream sstm;
+            sstm << num << j;
+            num = "00"+sstm.str();
+        }else {
+            stringstream sstm;
+            sstm << num << j;
+            num = "0"+sstm.str();
+        }
+        
+        trainPath = trainPath+ "/image_"+num+".jpg";
+        imgDeneme = imread(trainPath);
+        
+        if(imgDeneme.empty()){
+            //cout << "not Train image : " << categories[i] << num << endl ;
+        }else{
+            
+            //To store the keypoints that will be extracted by SIFT
+            vector<KeyPoint> keypoints;
+            //To store the SIFT descriptor of current image
+            Mat descriptor;
+            //The SIFT feature extractor and descriptor
+            cv::xfeatures2d::SiftDescriptorExtractor detector;
+            
+            
+            
+            //SIFT BASLANGIC
+            Ptr<Feature2D> f2d = xfeatures2d::SIFT::create();
+            
+            f2d->detect(imgDeneme, keypoints);
+            //compute the descriptors for each keypoint
+            f2d->compute(imgDeneme, keypoints,descriptor);
+            
+            featuresUnclustered.push_back(descriptor);
+            
+
+        }
+        
+    }
+    
+    //Construct BOWKMeansTrainer
+    //the number of bags
+    int dictionarySize=50;
+    //define Term Criteria
+    TermCriteria tc(CV_TERMCRIT_ITER,100,0.001);
+    //retries number
+    int retries=1;
+    //necessary flags
+    int flags=KMEANS_PP_CENTERS;
+    //Create the BoW (or BoF) trainer
+    BOWKMeansTrainer bowTrainer(dictionarySize,tc,retries,flags);
+    //cluster the feature vectors
+    Mat dictionary=bowTrainer.cluster(featuresUnclustered);
+    //store the vocabulary
+    FileStorage fs("dictionary.yml", FileStorage::WRITE);
+    fs << "vocabulary" << dictionary;
+    fs.release();
+    
+    
+
+    
+    Mat imgDeneme2;// open image
+    imgDeneme2 = imread("/Users/omer/Downloads/project3-data/test/forest/image_0215.jpg");			// open image
+    
+    
+    
+    
+    
+    
+    //
+    
+    
+    
+    //Step 2 - Obtain the BoF descriptor for given image/video frame.
+    
+    //prepare BOW descriptor extractor from the dictionary
+    Mat dictionary1;
+    FileStorage fs1("dictionary.yml", FileStorage::READ);
+    fs1["vocabulary"] >> dictionary1;
+    fs1.release();
+    
+    //To store the image file name
+    char * filename = new char[100];
+    //To store the image tag name - only for save the descriptor in a file
+    char * imageTag = new char[10];
+    
+    //open the file to write the resultant descriptor
+    FileStorage fs2("descriptor.yml", FileStorage::WRITE);
+    
+    Ptr<Feature2D> f2d2 = xfeatures2d::SIFT::create();
+    //To store the keypoints that will be extracted by SIFT
+    vector<KeyPoint> keypoints1;
+    //Detect SIFT keypoints (or feature points)
+    f2d2->detect(imgDeneme2,keypoints1);
+    //To store the BoW (or BoF) representation of the image
+    Mat bowDescriptor;
+    //extract BoW (or BoF) descriptor from given image
+    f2d2->compute(imgDeneme2,keypoints1,bowDescriptor);
+    
+    //prepare the yml (some what similar to xml) file
+    sprintf(imageTag,"img1");
+    //write the new BoF descriptor to the file
+    fs2 << imageTag << bowDescriptor;
+    
+    //You may use this descriptor for classifying the image.
+    
+    
+    
+    ///KNN Implement edilecek
+    FlannBasedMatcher matcher;
+    std::vector< DMatch > matches;
+    matcher.match(dictionary1, bowDescriptor, matches);
+
+    //////
+    
+    
+    
+    
+    cout << (float)matches.size() << endl;
+    //release the file storage
+    fs2.release();
+    
+
+    waitKey(0);
+    //SIFT BITIS
+
+}
+
+
 int main() {
 
 	int num_categories = 15;
@@ -23,26 +166,10 @@ int main() {
 
 	string categories[15] = {"bedroom","Coast","Forest","Highway","industrial","Insidecity","kitchen","livingroom","Mountain","Office","OpenCountry","store","Street","Suburb","TallBuilding"};
 	string testCategories[15] = {"bedroom","coast","forest","highway","industrial","insidecity","kitchen","livingRoom","mountain","office","opencountry","store","street","suburb","tallbuilding"};
-	
-	Mat imgDeneme;
-	Mat imgDeneme2;
-	imgDeneme = imread("/Users/omer/Downloads/project3-data/train/bedroom/image_0001.jpg");			// open image
-	imgDeneme2 = imread("/Users/omer/Downloads/project3-data/test/bedroom/image_0003.jpg");			// open image
-   
-    
-    
-    
-    //SIFT BASLANGIC
-    Ptr<Feature2D> f2d = xfeatures2d::SIFT::create();
-    vector<KeyPoint> keypoints_1;
-    f2d->detect(imgDeneme, keypoints_1 );
-    Mat output;
-    drawKeypoints(imgDeneme, keypoints_1, output);
-    imshow("Output", output);
-    waitKey(0);
-    //SIFT BITIS
-   
+	   
 
+    trainBOW();
+    
 	string trainPath = "";
 	string testPath = "";
 
@@ -51,6 +178,10 @@ int main() {
 	
 	char numstr[21];
 
+    
+    Mat imgDeneme;
+    Mat imgDeneme2;
+    
 	for(int i=0; i<num_categories;i++){
 		for(int j=1; j<num_images;j++){
 			trainPath = "/Users/omer/Downloads/project3-data/train/";
@@ -72,7 +203,6 @@ int main() {
             }
             
 			trainPath = trainPath + categories[i]+"/image_"+num+".jpg";
-            cout << trainPath << endl;
 			imgDeneme = imread(trainPath);
 			testPath = testPath + testCategories[i]+"/image_"+num+".jpg";
 			imgDeneme2 = imread(testPath);
